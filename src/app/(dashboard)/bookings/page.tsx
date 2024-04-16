f221a0e5f29051d82a0f3dccb0358323ebcc0f56
bhlox <kurkboard@gmail.com>
2024-04-16 09:35:00 +0800

    test hostname

--- src/app/(dashboard)/bookings/page.tsx
+++ src/app/(dashboard)/bookings/page.tsx
@@ -3,12 +3,16 @@ import React from "react";
 import DataTable from "@/components/bookings/data-table/index";
 import Headings from "@/components/ui/headings";
 import { getUser } from "@/lib/actions/auth";
+import { headers } from "next/headers";
 
 export default async function BookingsPage() {
   const user = await getUser();
   if (!user || !user.user) {
     throw new Error("User not found");
   }
+  const headersList = headers();
+  const hostname = headersList.get("x-forwarded-host");
+  console.log({ hostname });
 
   const bookings = await getBookings({
     handlerId: user.user.id,
